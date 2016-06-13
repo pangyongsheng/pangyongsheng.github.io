5ffc29a2a601c669528f7592693685c4d8695160
pangyongsheng <pang_yongsheng@oulook.com>
2016-06-13 09:52:46 +0800

    all

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿$(function(){
+	$("#navigation li").each(function(){
+		if(this.className.indexOf("current_page")==-1){
+			$(this).find("a").css("left","-280px");
+			$(this).hover(function(){
+				//alert($("a",this));
+				$(this).find("a").animate({left:"0px"},"fast");
+			},function(){
+				$(this).find("a").animate({left:"-280px"},"fast");
+			})
+		}
+	})
+})
