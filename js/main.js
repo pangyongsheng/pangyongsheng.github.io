c7f3415ca75a84784ca5af6c793407d1cfb81e3c
pangyongsheng <pang_yongsheng@oulook.com>
2016-06-22 18:24:14 +0800

    a

--- js/main.js
+++ js/main.js
@@ -31,12 +31,17 @@ $(function(){
 	$("#head").mouseover(function(){
 		$("#menu").show();
 	});
-//
+	
+	contentLoad();
+//onload over
 });
+//window resize event
 window.onresize = function(){
 	var temp2=$("#first").css("display");//是否可见
 	if(temp2)	adpaHeight();
 }
+//function
+//scroll event
 var scrollFunc=function(ev)
 {
 	var oEvent=ev||windows.event;
@@ -45,15 +50,13 @@ var scrollFunc=function(ev)
 		if(oEvent.wheelDelta > 0)
 		{
 			 $("#menu").show();
-			  topUp();
-			 
+			  topUp(); 
 		}
 		if(oEvent.wheelDelta < 0)
 		{
 			hidef();
 			$("#menu").hide();
-			topDown();
-			
+			topDown();	
 		}
 	}
 	else if(oEvent.detail)
@@ -70,7 +73,8 @@ var scrollFunc=function(ev)
 			topDown();
 		}
 	}
-}	
+}
+//welcome page hidden
 function hidef()
 {
 	var first=document.getElementById("first");
@@ -81,11 +85,13 @@ function hidef()
 		"opacity":"0",
 	})
 }
+//adpative Height window
 function adpaHeight()
 {
 	 var bodyHeight = document.documentElement.clientHeight;			//获取当前浏览器宽高
     document.getElementById("first").style.height = (parseInt(bodyHeight)) + 'px';		//设置当前div宽高
 }
+//right image transition 
 function topDown(){
 	var oSideR=document.getElementById("side_right");
 	var oSideRT=oSideR.offsetTop+20;
@@ -103,4 +109,27 @@ function topUp(){
 	{
 		"top":"-600"
 	})
+}
+//text onload by ajax
+function contentLoad()
+{
+	//alert("1");
+	ajax('record.json?t='+new Date().getTime(),
+		function(str)
+		{
+			var arr=eval(str);
+			for(var i=0;i<arr.length;i++)
+			{
+				var temp=i+1;
+				$("#article_i:eq(0)").clone().appendTo($("#left"));
+				$(".article:eq("+temp+")").find("span:eq(0)").html(arr[i].name);
+				$(".article:eq("+temp+")").find("span:eq(1)").html(arr[i].time);
+				$(".article:eq("+temp+")").find("span:eq(2)").html(arr[i].word);	
+			}
+		},
+		function()
+		{
+			alert("额 出错啦(⊙o⊙)…");
+		}
+	);
 }
